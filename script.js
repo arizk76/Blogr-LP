8aad5ebca90d65115d3f79c823dbe3ade709be23
Ahmed Rizk <arizkcisco@gmail.com>
2021-03-29 05:10:38 +0200

    Header and hero fine tuning with different breakpoints and javascript

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const productDropDown = document.getElementById('product-dropdown');
+const productDownArrow = document.getElementById('product-dropdown');
 const productDropDownMenu = document.getElementById('product-drop-menu');
 const productLink = document.getElementById('product-link')
 
-const companyDropDown = document.getElementById('company-dropdown');
+const companyDownArrow = document.getElementById('company-dropdown');
 const companyDropDownMenu = document.getElementById('company-drop-menu');
 const companyLink = document.getElementById('company-link')
 
-const connectDropDown = document.getElementById('connect-dropdown');
+const connectDownArrow = document.getElementById('connect-dropdown');
 const connectDropDownMenu = document.getElementById('connect-drop-menu');
 const connectLink = document.getElementById('connect-link')
 
@@ -16,18 +16,18 @@ const connectLink = document.getElementById('connect-link')
 productLink.addEventListener('click', () => {
     productDropDownMenu.classList.toggle('show');
     productLink.classList.toggle('underline');
-    productDropDown.classList.toggle('rotate');
+    productDownArrow.classList.toggle('rotate');
 });
 
 companyLink.addEventListener('click', () => {
     companyDropDownMenu.classList.toggle('show');
     companyLink.classList.toggle('underline');
-    companyDropDown.classList.toggle('rotate');
+    companyDownArrow.classList.toggle('rotate');
     
 });
 
 connectLink.addEventListener('click', () => {
     connectDropDownMenu.classList.toggle('show');
     connectLink.classList.toggle('underline');
-    connectDropDown.classList.toggle('rotate');
+    connectDownArrow.classList.toggle('rotate');
 });
